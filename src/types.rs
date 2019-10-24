f1c065d8f3e2e71b0ea5c701f1f365128f1db451
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-10-24 19:49:27 +0200

    Update all types to use references

--- src/types.rs
+++ src/types.rs
@@ -1,69 +1,74 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct Params {
-    pub block: BlockInfo,
-    pub message: MessageInfo,
-    pub contract: ContractInfo,
+pub struct Params<'a> {
+    #[serde(borrow)]
+    pub block: BlockInfo<'a>,
+    #[serde(borrow)]
+    pub message: MessageInfo<'a>,
+    #[serde(borrow)]
+    pub contract: ContractInfo<'a>,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct BlockInfo {
+pub struct BlockInfo<'a> {
     pub height: i64,
     // time is seconds since epoch begin (Jan. 1, 1970)
     pub time: i64,
-    pub chain_id: String,
+    pub chain_id: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct MessageInfo {
-    pub signer: String,
-    pub sent_funds: Vec<Coin>,
+pub struct MessageInfo<'a> {
+    pub signer: &'a str,
+    #[serde(borrow)]
+    pub sent_funds: &'a [Coin<'a>],
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct ContractInfo {
-    pub address: String,
-    pub balance: Vec<Coin>,
+pub struct ContractInfo<'a> {
+    pub address: &'a str,
+    #[serde(borrow)]
+    pub balance: &'a [Coin<'a>],
 }
 
 #[derive(Serialize, Deserialize, Clone)]
-pub struct Coin {
-    pub denom: String,
-    pub amount: String,
+pub struct Coin<'a> {
+    pub denom: &'a str,
+    pub amount: &'a str,
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
-pub enum CosmosMsg {
+pub enum CosmosMsg<'a> {
     // this moves tokens in the underlying sdk
     Send {
-        from_address: String,
-        to_address: String,
-        amount: Vec<Coin>,
+        from_address: &'a str,
+        to_address: &'a str,
+        amount: &'a [Coin<'a>],
     },
     // this dispatches a call to another contract at a known address (with known ABI)
     // msg is the json-encoded HandleMsg struct
     Contract {
-        contract_addr: String,
-        msg: String,
+        contract_addr: &'a str,
+        msg: &'a str,
     },
     // this should never be created here, just passed in from the user and later dispatched
     Opaque {
-        data: String,
+        data: &'a str,
     },
 }
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
-pub enum ContractResult {
-    Ok(Response),
-    Err(String),
+pub enum ContractResult<'a> {
+    Ok(Response<'a>),
+    Err(&'a str),
 }
 
-impl ContractResult {
+impl<'a> ContractResult<'a> {
     // unwrap will panic on err, or give us the real data useful for tests
-    pub fn unwrap(self) -> Response {
+    pub fn unwrap(self) -> Response<'a> {
         match self {
             ContractResult::Err(msg) => panic!("Unexpected error: {}", msg),
             ContractResult::Ok(res) => res,
@@ -79,37 +84,37 @@ impl ContractResult {
 }
 
 #[derive(Serialize, Deserialize, Default)]
-pub struct Response {
+pub struct Response<'a> {
     // let's make the positive case a struct, it contrains Msg: {...}, but also Data, Log, maybe later Events, etc.
-    pub messages: Vec<CosmosMsg>,
-    pub log: Option<String>,
-    pub data: Option<String>,
+    pub messages: &'a [CosmosMsg<'a>],
+    pub log: Option<&'a str>,
+    pub data: Option<&'a str>,
 }
 
 // just set signer, sent funds, and balance - rest given defaults
 // this is intended for use in testcode only
-pub fn mock_params(signer: &str, sent: &[Coin], balance: &[Coin]) -> Params {
+pub fn mock_params<'a>(signer: &'a str, sent: &'a [Coin<'a>], balance: &'a [Coin<'a>]) -> Params<'a> {
     Params {
         block: BlockInfo {
             height: 12345,
             time: 1571797419,
-            chain_id: "cosmos-testnet-14002".to_string(),
+            chain_id: "cosmos-testnet-14002",
         },
         message: MessageInfo {
-            signer: signer.to_string(),
-            sent_funds: sent.to_vec(),
+            signer: signer,
+            sent_funds: sent,
         },
         contract: ContractInfo {
-            address: "cosmos2contract".to_string(),
-            balance: balance.to_vec(),
+            address: "cosmos2contract",
+            balance: balance,
         },
     }
 }
 
 // coin is a shortcut constructor for a set of one denomination of coins
-pub fn coin(amount: &str, denom: &str) -> Vec<Coin> {
+pub fn coin<'a>(amount: &'a str, denom: &'a str) -> Vec<Coin<'a>> {
     vec![Coin {
-        amount: amount.to_string(),
-        denom: denom.to_string(),
+        amount: amount,
+        denom: denom,
     }]
 }
